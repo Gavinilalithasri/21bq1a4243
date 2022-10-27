b5ce364084ac93f3814247aca656550b38ad673d
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-10-27 20:55:02 +0530

    Add files via upload

--- NewThread.java
+++ NewThread.java
@@ -1,19 +1,24 @@
 package JAVA;
 
 public class NewThread implements Runnable {
-	NewThread() {
-	   Thread t=new Thread();
+	String name;
+	Thread t;
+	NewThread(String threadname) {
+		name=threadname;
+	    t=new Thread(this,name);
+	    System.out.println("new thread:"+t);
 	}
 	public void run() {
 		try {
 			for(int i=10;i>5;i--) {
-				System.out.println(i);
-				Thread.sleep(500);
+				System.out.println(name+";"+i);
+				Thread.sleep(1000);
 			}
 		}
 		catch(InterruptedException e) {
-			System.out.println(e);
+			System.out.println(name+"interrupted");
 		}
+		System.out.println(name+"exiting");
 	}
 	
 
