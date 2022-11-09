d6eb31b6b0a96754964656b1f38547b5a75de399
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-11-09 19:54:11 +0530

    Add files via upload

---
+++ Dollar.java
@@ -0,0 +1,12 @@
+package JAVA;
+
+public class Dollar {
+   public void DollarToINR(double dollar) {
+	 System.out.println("$ "+dollar +" in INR : Rs." + dollar*76);
+	 }
+	public void INRtoDollar(double inr) {
+	 System.out.println("Rs."+inr +" in Dollars : $" + inr/76);
+	 
+
+}
+}
