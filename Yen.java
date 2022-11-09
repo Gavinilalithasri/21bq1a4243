df6777fb8dca40c36ecbd47e0feef286c41e9e38
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-11-09 19:56:00 +0530

    Add files via upload

---
+++ Yen.java
@@ -0,0 +1,11 @@
+package JAVA;
+
+public class Yen {
+	public void YenToINR(double yen) {
+		 System.out.println(yen +" Yen in INR : Rs." + yen*0.64);
+		 }
+		public void INRtoYen(double inr) {
+		 System.out.println("Rs."+inr +" in Yen : " + inr/0.64 + " yen");
+		 }
+
+}
