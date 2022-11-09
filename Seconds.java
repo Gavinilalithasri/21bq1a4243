1d021dd1e0c184b2b70d5eba96852717e15a417d
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-11-09 19:58:19 +0530

    Add files via upload

---
+++ Seconds.java
@@ -0,0 +1,11 @@
+package Time;
+
+public class Seconds {
+	public void secsToHours(double secs) {
+		 System.out.println(secs +" sec in Hours : " + secs/(3600) + " hr");
+		 }
+		public void hoursToSecs(double hour) {
+		 System.out.println(hour +" hr in Seconds : " + hour*60*60 +" sec");
+		 }
+
+}
