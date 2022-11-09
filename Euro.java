5fa0e399ca4caea1e4c3f77035640708c6081af2
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-11-09 19:55:36 +0530

    Add files via upload

---
+++ Euro.java
@@ -0,0 +1,11 @@
+package JAVA;
+
+public class Euro {
+	public void EuroToINR(double euro) {
+		 System.out.println(euro +" Euros in INR : Rs." + euro*84);
+		 }
+		public void INRtoEuro(double inr) {
+		 System.out.println("Rs."+ inr +" in Euros : " + inr/84+" Euro");
+		 }
+
+}
