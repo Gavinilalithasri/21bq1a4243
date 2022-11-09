60b87b00d7154ab8d47c6eb645c5d4a5b2a4aa0f
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-11-09 19:59:07 +0530

    Add files via upload

---
+++ Miles.java
@@ -0,0 +1,11 @@
+package Distance;
+
+public class Miles {
+	public void MilesToKM(double miles) {
+		 System.out.println(miles +" miles in KM : " + miles*1.6 +" km");
+		 }
+		public void KMtoMiles(double km) {
+		 System.out.println(km +" KM in Miles : "+ km/1.6 +" miles");
+		 }
+
+}
