0e15e69e7de348de9b92d38eddb007ff8c9535c6
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-11-09 19:57:52 +0530

    Add files via upload

---
+++ Minutes.java
@@ -0,0 +1,11 @@
+package Time;
+
+public class Minutes {
+	public void minsToHours(double mins) {
+		 System.out.println(mins +" mins in Hours : " + mins/60 + " hr");
+		 }
+		public void hoursToMins(double hour) {
+		 System.out.println(hour +" hr in Minutes : " + hour*60 +" min");
+		 }
+
+}
