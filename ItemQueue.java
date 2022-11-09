62a89d7a26d7d1eec3cc0ae2e5d4d402ead6c0c5
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-11-09 19:29:56 +0530

    Add files via upload

---
+++ ItemQueue.java
@@ -0,0 +1,46 @@
+package JAVA;
+
+public class ItemQueue {
+	int item;
+	boolean valueSet = false;
+	synchronized int getItem() {
+	 while (!valueSet)
+	 try {
+	 wait();
+	 }
+	 catch (InterruptedException e) {
+	 System.out.println("InterruptedException caught");
+	 }
+	 System.out.println("Consumed:" + item);
+	 valueSet = false;
+	 try {
+	 Thread.sleep(1000);
+	 }
+	 catch (InterruptedException e) {
+	 System.out.println("InterruptedException caught");
+	 }
+	 notify();
+	 return item;
+	}
+	synchronized void putItem(int item) {
+	 while (valueSet)
+	 try {
+	 wait();
+	 }
+	 catch (InterruptedException e) {
+	 System.out.println("InterruptedException caught");
+	 }
+	 this.item = item;
+	 valueSet = true;
+	 System.out.println("Produced: " + item);
+	 try {
+	 Thread.sleep(1000);
+	 }
+	 catch (InterruptedException e) {
+	 System.out.println("InterruptedException caught");
+	 }
+	 notify();
+	 } 
+
+
+}
