e689a671de10afea0f4a63bda42dd242e5abf0e3
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-11-09 19:58:49 +0530

    Add files via upload

---
+++ Meter.java
@@ -0,0 +1,11 @@
+package Distance;
+
+public class Meter {
+	public void MeterToKM(double meter) {
+		 System.out.println(meter +" m in KM : " + meter/1000 + " KM");
+		 }
+		public void KMtoMeter(double km) {
+		 System.out.println(km +" KM in Meters : " + km*1000 +" m");
+		 }
+
+}
